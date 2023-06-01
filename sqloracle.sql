dc94616e45325ef427900539ce83badac31ecea4
Richard Collao <richard.collao@outlook.cl>
2023-06-01 15:57:36 -0400

    Update sqloracle.sql

--- sqloracle.sql
+++ sqloracle.sql
@@ -1,1 +1,2 @@
-SELECT TO_DATE('01/06/2023', 'dd/mm/yyyy') - INTERVAL '16' MONTH AS resultado FROM dual;
+SELECT TO_DATE('01/06/2023', 'dd/mm/yyyy') - INTERVAL '6' MONTH AS resultado FROM dual;
+SELECT DISTINCT columna FROM()
