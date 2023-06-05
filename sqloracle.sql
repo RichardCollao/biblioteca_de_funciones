78951cfe05c4e3cfeec1aca0e1258dcdcbb7280b
Richard Collao <richard.collao@outlook.cl>
2023-06-05 09:36:45 -0400

    Update sqloracle.sql

--- sqloracle.sql
+++ sqloracle.sql
@@ -1,3 +1,7 @@
 SELECT TO_DATE('01/06/2023', 'dd/mm/yyyy') - INTERVAL '6' MONTH AS result FROM dual;
 SELECT DISTINCT column FROM()
 SELECT * FROM table WHERE column IS NOT NULL;
+
+DELETE FROM myTable;
+TRUCATE TABLE myTable;// borra todos los registros y resetea el valor del campo autoincrementable
+DBCC CHECKIDENT ('myTable', RESEED, 1000);// setea el valor autoincrementable
