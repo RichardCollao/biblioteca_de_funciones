23969e0ab0b627cf13a85d34dbe65ff2c4bcf5ba
Richard Collao <richard.collao@outlook.cl>
2023-06-05 16:05:14 -0400

    Update sqloracle.sql

--- sqloracle.sql
+++ sqloracle.sql
@@ -12,3 +12,26 @@ IF OBJECT_ID('myTable', 'U') IS NOT NULL
 SET IDENTITY_INSERT myTable ON 
 INSERT INTO ...
 SET IDENTITY_INSERT myTable OFF
+
+DATEADD(DAY, -1, CONVERT(DATE, '2023-06-05', 120))
+Code |   Result
+-----|-----------------
+ 101 |   mm/dd/yyyy
+ 102 |   yyyy.mm.dd
+ 103 |   dd/mm/yyyy
+ 104 |   dd.mm.yyyy
+ 105 |   dd-mm-yyyy
+ 106 |   dd mon yyyy
+ 107 |   Mon dd, yyyy
+ 108 |   hh:mi:ss
+ 109 |   mon dd yyyy hh:mi:ss:mmmAM (or PM)
+ 110 |   mm-dd-yyyy
+ 111 |   yyyy/mm/dd
+ 112 |   yyyymmdd
+ 113 |   dd mon yyyy hh:mi:ss:mmm(24h)
+ 114 |   hh:mi:ss:mmm(24h)
+ 120 |   yyyy-mm-dd hh:mi:ss(24h)
+ 121 |   yyyy-mm-dd hh:mi:ss.mmm(24h)
+ 126 |   yyyy-mm-ddThh:mi:ss.mmm (no spaces)
+ 130 |   dd mon yyyy hh:mi:ss:mmmAM
+ 131 |   dd/mm/yyyy hh:mi:ss:mmmAM
