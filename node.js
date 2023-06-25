ab93dd3bfeec9ee17f7d181d20da093129e2ab28
Richard Collao <richard.collao@outlook.cl>
2023-06-24 22:41:53 -0400

    Update node.js

--- node.js
+++ node.js
@@ -1,17 +1,23 @@
 #WEBPACK
-#  construye y empaqueta el proyecto según la configuración del archivo webpack.config.js
+// construye y empaqueta el proyecto según la configuración del archivo webpack.config.js
 npx webpack
 
 #NODE
-# ejecuta la aplicación
+// ejecuta la aplicación
 node app.js
 
+
+
 #NPM
-#  ejecuta la aplicación, requiere definir "start" en el archivo package.json "scripts": {"start": "node app.js"}
+// ejecuta la aplicación, requiere definir "start" en el archivo package.json "scripts": {"start": "node app.js"}
 npm start
 
-# mostrarán el árbol de dependencias instaladas en tu proyecto. npm list o npm ls
+// mostrarán el árbol de dependencias instaladas en tu proyecto. npm list o npm ls
 npm list
 
-# mostrará solo las librerías de nivel superior sin las dependencias anidadas.
+// mostrará solo las librerías de nivel superior sin las dependencias anidadas.
 npm list --depth=0
+
+// desinstalar la librería
+npm uninstall nombre-libreria
+
