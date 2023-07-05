bd5c0a36f3f054df7a289fe4d5cfc75cfbe2dfa0
Richard Collao <richard.collao@outlook.cl>
2023-07-05 08:44:00 -0400

    Update codeigniter4.php

--- codeigniter4.php
+++ codeigniter4.php
@@ -4,3 +4,6 @@
 
 // [emergency, alert, critical, error, warning, notice, info, or debug] ($level (string), $message (string), $context (array))
 log_message('debug', $sql . PHP_EOL);
+
+// 
+Services::logger()->info('Este es un mensaje de información');
