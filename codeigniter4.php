79c945c6ccd811a49dde4cc651f2473141dd17eb
Richard Collao <richard.collao@outlook.cl>
2023-07-06 18:45:50 -0400

    Update codeigniter4.php

--- codeigniter4.php
+++ codeigniter4.php
@@ -1,7 +1,8 @@
 <?php
 // QUERY BUILDER
-->getCompiledSelect();// devuelve un string de la consulta sql
-
+$builder->getCompiledSelect();// devuelve un string de la consulta sql
+$db->getLastQuery();
+  
 // [emergency, alert, critical, error, warning, notice, info, or debug] ($level (string), $message (string), $context (array))
 log_message('debug', $sql . PHP_EOL);
 
